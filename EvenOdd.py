f789e0c2135ca87f515252773fc562eada7010b5
Mayur8933 <mp326395@gmail.com>
2020-06-11 10:47:06 +0530

    adding two files

---
+++ EvenOdd.py
@@ -0,0 +1,23 @@
+#write a program to check if given number is odd or even
+
+def isEvenOdd(number):
+    if number % 2 == 0:
+        print(number,"is even")
+    else:
+        print(number,"is odd")
+isEvenOdd(3)        
+
+for i in range(0,5):
+    isEvenOdd(i)
+
+for countdown in 5,10,3,1:
+    print(countdown)
+
+    
+#how to create a list
+cities = ["mumbai","pune","New York"]
+for city in cities:
+    print(city)
+    
+
+        
---
+++ py1.py
@@ -0,0 +1,25 @@
+print("hello world!")
+
+x=5
+print(x)
+print(type(x))
+
+x="python language"
+print(x)
+print(type(x))
+
+x=7.5
+print(x)
+print(type(x))
+
+x="A"
+x="python"
+print(x)
+print(type(x))
+
+x= True
+print(x)
+print(type(x))
+
+
+
